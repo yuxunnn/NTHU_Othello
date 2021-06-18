5a50a6d0e2c2965c33c54b75ffdd8c445473c59c
yuxunnn <yuxun90140@gmail.com>
2021-06-19 02:21:46 +0800

    something

--- player_yuxun.cpp
+++ player_yuxun.cpp
@@ -5,10 +5,12 @@
 #include <cstdlib>
 #include <ctime>
 #include <set>
+
+// Declare
 struct Point {
     int x, y;
     Point() : Point(0, 0) {}
-	Point(float x, float y) : x(x), y(y) {}
+	Point(int x, int y) : x(x), y(y) {}
 	bool operator==(const Point& rhs) const {
 		return x == rhs.x && y == rhs.y;
 	}
@@ -25,16 +27,33 @@ struct Point {
 
 int player;
 const int SIZE = 8;
-std::array<std::array<int, SIZE>, SIZE> board;
 std::vector<Point> next_valid_spots;
-
 using State = std::array<std::array<int, SIZE>, SIZE>;
+State board;
 
 const std::array<Point, 8> directions{{
     Point(-1, -1), Point(-1, 0), Point(-1, 1),
     Point(0, -1), /*{0, 0}, */Point(0, 1),
     Point(1, -1), Point(1, 0), Point(1, 1)
 }};
+struct Node{
+    State _s;
+    int value;
+    std::set<Node*> child;
+    std::vector<Point> valid_spots;
+};
+
+bool is_spot_on_board(Point p);
+bool is_disc_at(State s, Point p, int disc);
+int state_value(State Board);
+std::vector<Point> get_valid_spots(State s, int curr_player);
+Node* make_node(State s);
+void build_tree(Node *curr, int curr_player);
+bool is_spot_valid(State s, Point center, int curr_player);
+State flip_board(State parent, Point center, int curr_player);
+int alpha_beta(Node *curr, int depth, int alpha, int beta, bool maximizingPlayer);
+
+
 bool is_spot_on_board(Point p){
     return 0 <= p.x && p.x < SIZE && 0 <= p.y && p.y < SIZE;
 }
@@ -43,23 +62,6 @@ bool is_disc_at(State s, Point p, int disc) {
     if (s[p.x][p.y] != disc) return false;
     return true;
 }
-struct Node{
-    State _s;
-    int value;
-    std::set<Node*> child;
-};
-
-Node* make_node(State s){
-    Node *temp = new Node();
-    temp->_s = s;
-    temp->value = state_value(s);
-    return temp;
-}
-
-Node* build_tree(Node *curr){
-
-}
-
 // Calculate state value
 int state_value(State Board){
     
@@ -67,8 +69,8 @@ int state_value(State Board){
 
     for (int i = 0; i < SIZE; i++){
         for (int j = 0; j < SIZE; j++){
-            if (Board[i][j] == 1) value += 1; // black
-            if (Board[i][j] == 2) value -= 1; // white
+            if (Board[i][j] == player) value += 1;
+            if (Board[i][j] == 3 - player) value -= 1;
         }
     }
 
@@ -89,28 +91,77 @@ int state_value(State Board){
     return value;
 }
 
-// Flip the board
-std::vector<int> dx = {-1, -1, -1, 0, 0, 1, 1, 1};
-std::vector<int> dy = {-1, 0, 1, -1, 1, -1, 0, 1};
-State flip_board(State parent, Point center, int curr_player) {
-    State s = parent;
+std::vector<Point> get_valid_spots(State s, int curr_player) {
+    std::vector<Point> valid_spots;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            Point p = Point(i, j);
+            if (s[i][j] != 0) continue;
+            if (is_spot_valid(s, p, curr_player)) valid_spots.push_back(p);
+        }
+    }
+    return valid_spots;
+}
+
+
+Node* make_node(State s){
+    Node *temp = new Node();
+    temp->_s = s;
+    temp->value = state_value(s);
+    return temp;
+}
+void build_tree(Node *curr, int curr_player){
+    
+    curr->valid_spots = get_valid_spots(curr->_s, curr_player);
+    
+    for (auto i : curr->valid_spots){
+        curr->child.insert(make_node(flip_board(curr->_s, i, curr_player)));
+
+    }
+
+    for(auto i : curr->child){
+        build_tree(i, 3 - curr_player);
+    }
+}
+bool is_spot_valid(State s, Point center, int curr_player) {
+    if (s[center.x][center.y] != 0) return false;
     for (Point dir: directions) {
         // Move along the direction while testing.
         Point p = center + dir;
         if (!is_disc_at(s, p, 3 - curr_player)) continue;
-        std::vector<Point> discs({p});
         p = p + dir;
         while (is_spot_on_board(p) && s[p.x][p.y] != 0) {
-            if (is_disc_at(s, p, curr_player)) {
+            if (is_disc_at(s, p, curr_player)) return true;
+            p = p + dir;
+        }
+    }
+    return false;
+}
+
+// Flip the board
+State flip_board(State parent, Point center, int curr_player) {
+    State ss = parent;
+    
+    for (Point dir: directions) {
+        // Move along the direction while testing.
+        Point p = center + dir;
+        if (!is_disc_at(ss, p, 3 - curr_player)) continue;
+        
+        std::vector<Point> discs({p});
+        p = p + dir;
+
+        while (is_spot_on_board(p) && ss[p.x][p.y] != 0) {
+            if (is_disc_at(ss, p, curr_player)) {
                 for (Point d: discs) {
-                    s[d.x][d.y] = curr_player;
+                    ss[d.x][d.y] = curr_player;
                 }
             }
             discs.push_back(p);
             p = p + dir;
         }
     }
-    return s;
+
+    return ss;
 }
 
 // Alpha-Beta pruning
@@ -126,7 +177,7 @@ int alpha_beta(Node *curr, int depth, int alpha, int beta, bool maximizingPlayer
             if (alpha >= beta) break;
         }
         return value;
-    }else {
+    }else { // minimizingPlayer
         int value = INT32_MAX;
         for (auto child : curr->child){
             value = std::min(value, alpha_beta(child, depth - 1, alpha, beta, true));
@@ -159,17 +210,29 @@ void read_valid_spots(std::ifstream& fin) {
 
 void write_valid_spot(std::ofstream& fout) {
 
-    int n_valid_spots = next_valid_spots.size();
+    // int n_valid_spots = next_valid_spots.size();
 
     // srand(time(NULL));
     // // Choose random spot. (Not random uniform here)
     // int index = (rand() % n_valid_spots);
     // Point p = next_valid_spots[index];
 
-    int maxVal;
-    Point p;
     Node *root = make_node(board);
-
+    build_tree(root, player);
+    
+    int maxChild, count = 0, id;
+    Point p;
+    for (auto it = root->child.begin(); it != root->child.end(); it++){
+        int value = alpha_beta(*it, 64, INT32_MIN, INT32_MAX, 3 - player);
+        if (it == root->child.begin() || value > maxChild){
+            maxChild = value;
+            id = count;
+        }
+        count++;
+    }
+    auto it = root->valid_spots.begin();
+    while(id--) it++;
+    p = *it;
 
     // Remember to flush the output to ensure the last action is written to file.
     fout << p.x << " " << p.y << std::endl;
